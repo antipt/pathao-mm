0e4ab000307c5a188fcb6df0e48ba313ad5d7e53
omartuhin-pathao <129460542+omartuhin-pathao@users.noreply.github.com>
2023-07-26 00:06:44 +0600

    Update pathao-mm.js

--- pathao-mm.js
+++ pathao-mm.js
@@ -1,1 +1,11 @@
-alert('success')
+     // Execute the code after the page has loaded
+        window.addEventListener("load", function() {
+            // Find the element with class "external-login-button-label"
+            const labelElement = document.querySelector(".external-login-button-label");
+            
+            // Check if the element exists and its text is "GitLab"
+            if (labelElement && labelElement.textContent === "GitLab") {
+                // Replace the text with "Google"
+                labelElement.textContent = "Google";
+            }
+        });
